71dd9281f6960ece14d1348bee6d7494f2fab04b
GikundiG <itsgikundimurithi@gmail.com>
2023-02-24 11:23:34 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main -> prints the numbers
- * Return:0
+ * main - function that checks for uppercase character.
+ * Return: 0
  */
 int main(void)
 {
-int i;
+	int h = 1;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
-}
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (i == 1)
-{
-printf("%d", i);
-}
-else
-{
-printf("%d", i);
-}
-}
-printf("\n");
+	while (h <= 100)
+	{
+		if (h % 3 == 0 && h % 5 == 0)
+			printf("FizzBuzz ");
+		else if (h % 5 == 0)
+		{
+			if (h == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (h % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", h);
+	h++;
+	}
 return (0);
 }
+
