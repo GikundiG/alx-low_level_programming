0cabd8b36cac684167cf521d5630b7e13faee8bf
GikundiG <itsgikundimurithi@gmail.com>
2023-03-02 10:31:14 -0800

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,34 +1,17 @@
 #include "main.h"
-
 /**
- * cap_string - capitalizes everey word of a string
- * @s: string to modify
+ * string_toupper -> from lower to upercase
+ * @m: character
+ * Return: s
  *
- * Return: the resulting string
  */
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
-	int i, j;
-
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
-		}
-	}
-
-	return (s);
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+}
+return (s);
 }
