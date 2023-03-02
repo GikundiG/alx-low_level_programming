baa0cae4933b49800f29383167adfb0786503a7c
GikundiG <itsgikundimurithi@gmail.com>
2023-03-02 11:07:12 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,32 @@
 #include "main.h"
+
 /**
- * cap_string -> capitalizes the strings provided
- * @s: string to be capitalizes
- * Return: (s)
+ * cap_string - Capitalizes each word in a string.
+ * @s: Pointer to string.
  *
+ * Return: Pointer to updated string.
  */
 char *cap_string(char *s)
 {
-int i, j;
-char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-for (i = 0; s[i] != '\0'; i++)
-{
-if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-	s[i] -= 32;
-for (j = 0; j < 13; j++)
-{
-if (s[i] == spe[j])
-{
-if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-{
-s[i + 1] -= 32;
+	int i;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n'
+		    || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.'
+		    || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"'
+		    || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{'
+		    || s[i - 1] == '}')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		}
+	}
+
+	return (s);
 }
-}
-}
-}
-return (s);
-}
+
