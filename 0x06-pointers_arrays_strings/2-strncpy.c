4411ff803eb0485fb1afb9b0932c651cdbe6710a
GikundiG <itsgikundimurithi@gmail.com>
2023-03-02 10:11:28 -0800

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy -> copies character
+ * @dest: - character arg
+ * @src: - character arg
+ * @n: - character arg
+ * Return: dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
