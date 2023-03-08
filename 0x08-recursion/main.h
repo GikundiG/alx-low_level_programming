fab71fa68be6d5b5b2c004c5ed5d2539d8439641
GikundiG <itsgikundimurithi@gmail.com>
2023-03-08 09:35:14 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int natural_numbers(int n, int i);
+int is_prime_number(int n);
+int prime(int n, int i);
+int is_palindrome(char *s);
+int palindrome(char *s, int i, int len);
+int strleni(char *s);
+int wildcmp(char *s1, char *s2);
+#endif
