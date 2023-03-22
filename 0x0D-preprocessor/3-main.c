a179ff5900ca65927a041c280f1d2edab25ac7fc
GikundiG <itsgikundimurithi@gmail.com>
2023-03-21 18:27:16 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
+}
+julien@ubuntu:~/0x0c. macro, structures$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c -o d
+julien@ubuntu:~/0x0c. macro, structures$ ./d 
+980, 980
+julien@ubuntu:~/0x0c. macro, structures$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x0D-preprocessor
+File: 3-function_like_macro.h
+   
+4. SUM
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y.
+
+julien@ubuntu:~/0x0c. macro, structures$ cat 4-main.c
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
